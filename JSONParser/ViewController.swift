f3cfcf8f2dc5418306aa4e39fc0627d201764a64
Trevor Beasty <trevor.beasty@weightwatchers.com>
2019-08-09 10:46:02 -0400

    Initial Commit

---
+++ JSONParser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONParser
+//
+//  Created by Trevor Beasty on 8/9/19.
+//  Copyright © 2019 Trevor Beasty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
